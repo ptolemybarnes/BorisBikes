4580655f473caaa1aed85d8457cb27bf1222d4dd
Ptolemy <ptolemybarnes@gmail.com>
2014-12-09 13:52:39 +0000

    add fix method

--- lib/borisbikes.rb
+++ lib/borisbikes.rb
@@ -1,7 +1,19 @@
 class Bike
-	def broken?
+	
+  def initialize
+    @broken = false
+  end
+
+  def broken?
+    @broken
   end
 
   def break!
+    @broken = true
   end
+
+  def fix!
+    @broken =false
+  end
+  
 end
