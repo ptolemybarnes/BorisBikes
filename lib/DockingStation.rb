af2d2340ceb842ec8a5bce9638cba7c10435b990
Ptolemy <ptolemybarnes@gmail.com>
2014-12-09 16:09:26 +0000

    Add DockingStation capacity and full? method.

---
+++ lib/DockingStation.rb
@@ -0,0 +1,25 @@
+class DockingStation
+  DEFAULT_CAPACITY = 20
+
+  def initialize(options = {})
+    @capacity = options.fetch(:capacity, DEFAULT_CAPACITY)
+    @bikes = []
+  end
+
+  def bike_count
+    @bikes.count
+  end
+
+  def dock(bike)
+    @bikes << bike
+  end
+
+  def release(bike)
+    @bikes.delete(bike)
+  end
+
+  def full?
+    @bikes.count == @capacity
+  end
+
+end
